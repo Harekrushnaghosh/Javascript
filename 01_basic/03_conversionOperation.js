21586917190f5b59bfb0c361a3fb3fd07a4c75f8
Harekrushna Ghosh <111685621+Harekrushnaghosh@users.noreply.github.com>
2024-01-21 17:13:17 +0000

    conversionofdatatypes

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "harekrushna"
+
+
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+
+//let valueInNumber = Number(score);
+//console.log(typeof  valueInNumber);
+//console.log(valueInNumber);
+
+//let isLoggedIn = "harekrushna"
+//let booleanIsLoggedIn = Boolean(isLoggedIn);
+//console.log(booleanIsLoggedIn);
+
+
+// 1 = true
+// 0 = false 
+// "harekrushna " = true 
+// empty = false 
+
+
+let someNumber = 33
+let stringSomeNumber = String(someNumber);
+console.log(stringSomeNumber);
+console.log( typeof stringSomeNumber);
+
+
