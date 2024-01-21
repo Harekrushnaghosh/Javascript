2fd38e8199eda09ca109a13f1ed809c79d2f2849
Harekrushna Ghosh <111685621+Harekrushnaghosh@users.noreply.github.com>
2024-01-21 16:44:43 +0000

    datatypes

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict";
+
+let name = "harekrushnaghosh"
+let number  = 8144224060
+let logedin = true
+let age = null
+let city;
+//console.log(typeof  name);
+//console.log(typeof  number);
+//console.log(typeof  logedin);
+//console.log(typeof  age);
+console.log(typeof  undefined);
+
+
+//typesofdatatypes
+//number
+//bigint
+//string = ""
+//boolean  = true/false
+// null = standalone value 
+// undefined = 
+//symbol
+
+
+//object
+
+
+
