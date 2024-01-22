9e1b0d5cd5cac049f8095c1640831b11fe31274e
Harekrushna Ghosh <111685621+Harekrushnaghosh@users.noreply.github.com>
2024-01-22 08:07:49 +0000

    dataTypessummary

---
+++ 01_basic/datatypes_summary.js
@@ -0,0 +1,48 @@
+// Primitive 
+
+// 7 types : String, Number, boolean, Null, undefined, Symbol, Bigint
+
+
+
+
+const score = 100 
+const scoreValue= null
+let userEmail;
+
+const Id = Symbol("1234");
+const anotherId = Symbol("1234");
+
+console.log (Id === anotherId)
+
+//const bigNumber = 12345678912345678n;
+
+
+// Reference  (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman" , "nagaraj", " doga"]
+
+
+let myobj = {
+    name : "harekrushna",
+    age : 23,
+
+}
+
+const myfunction = function(){
+    console.log("Hello world");
+
+}
+
+console.log(typeof anotherId);
+console.log(typeof score);
+console.log(typeof Id);
+console.log(typeof heros);
+console.log(typeof myobj);
+
+
+
+
+
+
