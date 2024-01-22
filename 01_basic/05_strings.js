21912325b89a1a56f08f4c38ed7953baa3903c33
Harekrushna Ghosh <111685621+Harekrushnaghosh@users.noreply.github.com>
2024-01-22 18:16:02 +0000

    comparision

---
+++ 01_basic/05_strings.js
@@ -0,0 +1,39 @@
+const name = "hare   kru shna"
+const repocount = 80
+
+console.log( name + repocount);
+
+
+
+console.log(`Hello my name is ${name} and my repo count is ${repocount}`);
+
+const mystate = new String ("odisha")
+
+console.log( mystate [2]);
+console.log( mystate.__proto__);
+console.log( mystate.length);
+console.log(mystate.toUpperCase());
+console.log( mystate.charAt("2"));
+console.log( mystate.indexOf("i"));
+
+
+
+const newString = mystate.substring(0,4);
+console.log(newString);
+
+const anotherString = mystate.slice(-6, 3);
+console.log(anotherString);
+
+const newStringOne = "     Odisha   "
+console.log(newStringOne );
+console.log(newStringOne.trim());
+
+
+
+const url = "https://google.com"
+console.log(url.replace("oo", "uu")
+);
+
+console.log(name.split(" "));
+
+
