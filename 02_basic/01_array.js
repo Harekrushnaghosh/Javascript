e77132e23dc319e20b9f543163944b85f8ddf821
Harekrushna Ghosh <111685621+Harekrushnaghosh@users.noreply.github.com>
2024-01-23 12:17:42 +0000

    arrayjs

---
+++ 02_basic/01_array.js
@@ -0,0 +1,38 @@
+//array
+
+const myArray = [0,1,2,3,4,5,6,7,8,9,"harekrushna"]
+//console.log(myArray[0]);
+
+//const myHeros= ["shaktiman", "naagraj"]
+
+//const myArray2 = new Array(1,2,3,4)
+//console.log(myArray2[3]);
+
+//Array Methods
+
+//myArray.push("Ghosh")
+//myArray.push("h")
+//myArray.push("h")
+
+//myArray.pop()
+myArray.unshift(9);
+myArray.shift();
+
+
+
+console.log(myArray);
+ 
+console.log(myArray.includes(9));
+console.log(myArray.indexOf(9));
+
+
+const newArr = myArray.join();
+console.log(myArray);
+console.log( typeof newArr);
+
+console.log("a", myArray);
+
+
+
+
+
