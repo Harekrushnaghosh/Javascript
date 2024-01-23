b8e93edf243a4006c6cd1de0af4a2b63c01fa89e
Harekrushna Ghosh <111685621+Harekrushnaghosh@users.noreply.github.com>
2024-01-23 14:44:12 +0000

    objects04

---
+++ 02_basic/04_object.js
@@ -0,0 +1,20 @@
+const tinderuser = {}
+tinderuser.id = "1234"
+tinderuser.name = "hare"
+tinderuser.loggedin = false
+
+//console.log(tinderuser);
+
+
+const regularuser = {
+    email: "some@gmail.com",
+    FullName:{
+        userFullName:{
+            FirstName: "harekrushna",
+            LastName: "Ghosh"
+        }
+
+    }
+}
+
+
